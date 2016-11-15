1066c3be6ffbe1302e3dbd68edb850fd9ec7e815
Hari Dharmawan <jc349594@ad.jcu.edu.au>
2016-11-15 14:37:58 +0800

    Initial commit

---
+++ src/MovieQuote.java
@@ -0,0 +1,11 @@
+/**
+ * Created by jc349594 on 15/11/16.
+ */
+import javax.swing.JOptionPane;
+public class MovieQuote
+{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "You die i die everybody die");
+    }
+}
---
+++ src/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+/**
+ * Created by jc349594 on 15/11/16.
+ */
+import javax.swing.JOptionPane;
+public class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "Maju hen ! MAJU, CSGO Overpass Movies, Fadni 'DeFaaLT' Harisyam, 2016");
+    }
+}
